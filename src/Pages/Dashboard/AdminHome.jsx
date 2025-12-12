e0e703424c49babf58910b89639502d611b2c8dd
HKrobinvai <hkrobin49@gmail.com>
2025-12-12 21:24:49 +0600

    added the admin home and show the payment and order and menu item coountity

--- src/Pages/Dashboard/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome.jsx
@@ -1,16 +1,78 @@
 import UserAuth from "../../hooks/UserAuth";
+import { useQuery } from "@tanstack/react-query";
 
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { FaDollarSign, FaJediOrder, FaUsers } from "react-icons/fa";
 
 const AdminHome = () => {
-    const {user} = UserAuth()
-    return (
-        <div>
-            <h2 className="text-3xl"><span>Hi, Welcome</span></h2>
-            {
-                user?.displayName ? user.displayName : "back"
-            }           
+  const { user } = UserAuth();
+  const axioSecure = useAxiosSecure();
+  const { data: stats } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axioSecure.get("/admin-stats");
+      return res.data;
+    },
+  });
+  return (
+    <div className="">
+      <div className="flex">
+        <h1 className="text-3xl ">
+        <span>Hi, Welcome</span>
+      </h1>
+      <h1>{user?.userName ? user.userName : "Back"}</h1>
+      </div>
+
+      <div className="stats shadow md:w-full bg-blue-100 mt-10">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+           <FaDollarSign className="text-3xl"></FaDollarSign>
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">{stats.revenue}</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
         </div>
-    );
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers className="text-3xl"></FaUsers>
+          </div>
+          <div className="stat-title">Users</div>
+          <div className="stat-value">{stats.users}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+           <FaJediOrder className="text-2xl"></FaJediOrder>
+          </div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stats.orders}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-8 w-8 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">Menu Items</div>
+          <div className="stat-value">{stats.menuItems}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminHome;
--- src/Pages/Dashboard/UserHome.jsx
+++ src/Pages/Dashboard/UserHome.jsx
@@ -1,16 +1,12 @@
-import UserAuth from "../../hooks/UserAuth";
+
+
 
 
 const UserHome = () => {
-    const {user} = UserAuth()
-    return (
-        <div>
-            <h1 className="text3xl"><span>Hi, Welcome</span></h1>            
-            {
-                user?.userName ? user.userName : "Back"
-            }
-        </div>
-    );
+ 
+  return (
+   <div></div>
+  );
 };
 
 export default UserHome;
